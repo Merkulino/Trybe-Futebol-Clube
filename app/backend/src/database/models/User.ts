07e2e42dfe776f2fe6404475f1a835f6cfb719dc
Melqui Brito <Merkulino11@gmail.com>
2023-05-11 15:35:59 -0300

    Feat: Criando a tabela User com Sequelize

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+export default class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.NUMBER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
