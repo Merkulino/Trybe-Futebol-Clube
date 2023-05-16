afdf34947668f8fb45712211492783d7ec33fb7a
Melqui Brito <Merkulino11@gmail.com>
2023-05-16 18:59:27 -0300

    Feat: Finalizando lógica de leaderboard

--- app/backend/src/api/service/leaderboard.service.ts
+++ app/backend/src/api/service/leaderboard.service.ts
@@ -18,14 +18,35 @@ const INCLUDE_HOME_AWAY_MATCH = {
   include: [includeMatches('homeMatch'), includeMatches('awayMatch')],
 };
 
+type TeamStringType = 'home' | 'away';
+
 // Função Total Points
 
 // Função Total Games
 
 export default class LeaderboardService {
-  public static countGoals(match: MatchGoalsType[], currency: 'home' | 'away'): number {
+  public static countGoals(match: MatchGoalsType[], currency: TeamStringType): number {
     const count = match.reduce((acc, curr) => acc + curr[`${currency}TeamGoals`], 0);
     return count;
+  }
+
+  public static countMatchesResult(
+    match: MatchGoalsType[],
+    team1: TeamStringType,
+    team2: TeamStringType,
+  ): number[] {
+    let drawMatch = 0;
+    let teamPoints = 0;
+    const count = match.reduce((acc, curr) => {
+      if (curr[`${team1}TeamGoals`] > curr[`${team2}TeamGoals`]) {
+        teamPoints += 3;
+        return acc + 1;
+      }
+      if (curr[`${team1}TeamGoals`] === curr[`${team2}TeamGoals`]) drawMatch += 1;
+      return acc + 0;
+    }, 0);
+    teamPoints += drawMatch;
+    return [count, drawMatch, teamPoints];
   }
 
   public static getGoalsFavor(team: TeamsMatchesType): number {
@@ -34,6 +55,29 @@ export default class LeaderboardService {
 
   public static getGoalsOwn(team: TeamsMatchesType): number {
     return this.countGoals(team.homeMatch, 'away') + this.countGoals(team.awayMatch, 'home');
+  }
+
+  /**
+   * @index 0 for get all Victories
+   * @index 1 for get all Draws
+   * @index 2 for get all Points
+  */
+  private static mapDataPointsResults(team: TeamsMatchesType, index: number): number {
+    return this.countMatchesResult(team.homeMatch, 'home', 'away')[index]
+      + this.countMatchesResult(team.awayMatch, 'away', 'home')[index];
+  }
+
+  public static getLosses(team: TeamsMatchesType): number {
+    return this.countMatchesResult(team.homeMatch, 'away', 'home')[0]
+      + this.countMatchesResult(team.awayMatch, 'home', 'away')[0];
+  }
+
+  public static getEfficencyTeam(team: TeamsMatchesType): number {
+    const efficiencyNumber = (
+      (this.mapDataPointsResults(team, 2)
+        / ((team.homeMatch.length + team.awayMatch.length) * 3)
+      ) * 100);
+    return Number((Math.round(efficiencyNumber * 100) / 100).toFixed(2));
   }
 
   public static async findAllTeamsGames(): Promise<TeamsMatchesType[]> {
@@ -46,15 +90,15 @@ export default class LeaderboardService {
 
     const newObj = response.map((team) => ({
       name: team.teamName,
-      totalPoints: 13,
+      totalPoints: this.mapDataPointsResults(team, 2),
       totalGames: team.homeMatch.length + team.awayMatch.length,
-      totalVictories: 4,
-      totalDraws: 1,
-      totalLosses: 0,
+      totalVictories: this.mapDataPointsResults(team, 0),
+      totalDraws: this.mapDataPointsResults(team, 1),
+      totalLosses: this.getLosses(team),
       goalsFavor: this.getGoalsFavor(team),
       goalsOwn: this.getGoalsOwn(team),
       goalsBalance: this.getGoalsFavor(team) - this.getGoalsOwn(team),
-      efficiency: 86.67,
+      efficiency: this.getEfficencyTeam(team),
     }));
 
     return { message: newObj };
